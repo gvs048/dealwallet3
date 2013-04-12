69967dd3da447c56848a334214a88f6fe90e6527
Tsss-Pc1 <Tsss-Pc1@Tsss-Pc1-PC>
2013-04-12 18:21:18 +0530

    commited on 04-12-2013 on 18:30

--- testCases/src/test/java/com/dealwallet/webdriver/selenium/dealwalletadmin/Category.java
+++ testCases/src/test/java/com/dealwallet/webdriver/selenium/dealwalletadmin/Category.java
@@ -1,18 +1,23 @@
-package selenium.webdriver.dealwalletadmin;
+package com.dealwallet.webdriver.selenium.dealwalletadmin;
+
+import static java.lang.Thread.sleep;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 
 public class Category
 {
 
 	WebDriver d;
+	private Select s;
 	@BeforeMethod
 	public void startup()
 	{
@@ -25,7 +30,7 @@ public class Category
 		d.quit();
 	}
 	@Test
-	public void testcategory()
+	public void testcategory() throws InterruptedException
 	{
 		d.get("http://localhost:8081/dealwalletadmin");
 		d.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -46,6 +51,23 @@ public class Category
 		}
 		//text.equalsIgnoreCase("You have entered an invalid username or password!")
 		d.findElement(By.xpath("//div[3]/div/div/p[1]/a")).click();
-
-	}
+		d.findElement(By.id("mer_id")).click();
+		s=new Select(d.findElement(By.id("mer_id")));
+		/*s.selectByIndex(9);
+		System.out.println("1");
+		Thread.sleep(20000);*/
+		s.selectByValue("134");
+		System.out.println("2");
+		sleep(20000);
+		/*s.selectByVisibleText("Tradus");
+		System.out.println("3");
+		Thread.sleep(20000);*/
+		/*s= new Select(d.findElement(By.name("category")));
+		s.selectByVisibleText("TV, Video and MP3 Players");
+		s= new Select(d.findElement(By.name("amCategory")));
+		s.selectByVisibleText("TVs, Audio/Video & Gaming");
+		d.findElement(By.cssSelector("input[type='submit']")).click();*/
+		Thread.sleep(20000);
+// this is a samp
+}
 }
